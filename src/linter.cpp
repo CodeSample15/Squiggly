b1ee9cfbf2892294355d9da506b2779d75470977
Luke Crimi <lukecrimi15@gmail.com>
2024-10-01 19:47:41 -0400

    More linter code

--- src/linter.cpp
+++ src/linter.cpp
@@ -1,1 +1,26 @@
 #include "linter.h"
+
+/*
+    Removes all whitespace from the program. Makes parsing the code easier and more reliable later on
+*/
+void Linter::preprocess(std::vector<std::string>& lines) 
+{
+    std::cout << "Preprocessing script...\t";
+
+    for(size_t i=0; i < lines.size(); i++) {
+        for(size_t j=0; j<lines[i].length(); j++) {
+            if(lines[i][j] == ' ' || lines[i][j] == '\t') {
+                //check to see if there is whitespace, that it's not being used to separate two distinct strings
+               bool rightGood = j+1 < lines[i].length() && !std::isalpha(lines[i][j+1]);
+               bool leftGood = j-1 >= 0 && !std::isalpha(lines[i][j-1]);
+
+                if(lines[i][j] == '\t' || rightGood || leftGood) {
+                    lines[i].erase(j, 1);
+                    j--;
+                }
+            }
+        }
+    }
+
+    std::cout << "Done" << std::endl;
+}
