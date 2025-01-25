1cd1fef464b47001fea08dca60f5b4ac1a7a0cb0
Luke Crimi <lukecrimi15@gmail.com>
2025-01-25 18:32:19 -0500

    debugging

--- src/frontend.cpp
+++ src/frontend.cpp
@@ -32,16 +32,16 @@ void throwFrontendError(std::string message);
     }
 
     void Frontend::drawScreen() {
-        //uint16_t color;
+        uint16_t color;
+        BuiltIn::Print("drawing screen");
         for(int x=0; x<SCREEN_WIDTH; x++) {
             for(int y=0; y<SCREEN_HEIGHT; y++) {
-                //color = myTFT.Color565(((int16_t)screen.screenBuff[y][x][0])<<8, ((int16_t)screen.screenBuff[y][x][1])<<8, ((int16_t)screen.screenBuff[y][x][2])<<8);
-                myTFT.IMDrawPixel(x, y, 0xFBFB);
+                color = myTFT.Color565(((int16_t)screen.screenBuff[y][x][0])<<8, ((int16_t)screen.screenBuff[y][x][1])<<8, ((int16_t)screen.screenBuff[y][x][2])<<8);
+                myTFT.IMDrawPixel(x, y, color);
             }
         }
 
         myTFT.IMDisplay();
-        TFT_MILLISEC_DELAY(1000);
     }
 
     float Frontend::getHorAxis() {
