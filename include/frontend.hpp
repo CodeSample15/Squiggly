df530f938deb1dfba607a1be0f837309ec4c222b
CodeSample15 <lukecrimi15@gmail.com>
2025-07-25 11:40:13 -0400

    revert to pc build for merge

--- include/frontend.hpp
+++ include/frontend.hpp
@@ -9,7 +9,7 @@
 #pragma once
 
 //this will allow both the raspi port and the pc port to be in the same codebase
-#define BUILD_FOR_RASPI true
+#define BUILD_FOR_RASPI false
 
 #if BUILD_FOR_RASPI
 #include "ST7735_TFT.hpp"
